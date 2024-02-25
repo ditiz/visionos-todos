e7387ce99470eb9cf9943f5636b2f7ad68faa0ae
Ditiz <mathieu.25565@gmail.com>
2024-02-25 01:34:10 +0100

    feat: create working todos list

--- Todo List/ContentView.swift
+++ Todo List/ContentView.swift
@@ -10,43 +10,48 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-
+    
     @State private var showImmersiveSpace = false
     @State private var immersiveSpaceIsShown = false
-
+    @State private var inputValue: String = ""
+    @State private var todos: [String] = []
+    
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
-
+    
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
-            Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding(.top, 50)
-        }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
+        VStack(spacing: 20) {
+            Text("Welcome to your todo list")
+            
+            TextField("Enter a task", text: $inputValue).frame(maxWidth: 200)
+            
+            Button(action: {
+                if !inputValue.isEmpty {
+                    todos.append(inputValue)
+                    inputValue = ""
                 }
+            }) {
+                Text("Add the todo")
             }
-        }
+            
+            
+            List {
+                ForEach(todos.indices, id: \.self) { index in
+                    HStack {
+                        Text(todos[index])
+                        Spacer()
+                        Button(action: {
+                            todos.remove(at: index)
+                        }) {
+                            Image(systemName: "xmark.circle.fill")
+                        }
+                    }
+                }
+            }.frame(maxWidth: 400)
+            
+            Model3D(named: "", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+        }.padding()
     }
 }
 
