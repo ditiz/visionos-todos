b1ad8b6cdbfa2299c90f42da8ae49a4cecbec0d3
Ditiz <mathieu.25565@gmail.com>
2024-02-25 12:31:33 +0100

    feat(todo-input): handle return key and focus

--- Todo List/TodoInputView.swift
+++ Todo List/TodoInputView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct TodoInputView: View {
     @Binding var inputValue: String
+    @FocusState var inputFocus: Bool
       var addTodo: () -> Void
       
       var body: some View {
-          TextField("Enter a task", text: $inputValue).frame(maxWidth: 200)
+          TextField("Enter a task", text: $inputValue)
+              .focused($inputFocus)
+              .frame(maxWidth: 200)
+              .onAppear {
+                  inputFocus = true
+              }.onSubmit() {
+                  addTodo()
+                  inputFocus = true
+              }
           
           Button(action: addTodo) {
               Text("Add the todo")
